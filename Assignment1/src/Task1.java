4cf9d9120e2b510b651606c56f9f7dc710323943
Chathurika <silvachathurtika@outlook.com>
2025-08-20 04:37:48 +0300

    Assignment1

--- Assignment1/src/Task1.java
+++ Assignment1/src/Task1.java
@@ -1,21 +1,8 @@
-import java.util.Scanner;
-
-public class Task1{
-
-
+public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Give the first number:");
-        int first = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Give the second number:");
-        int second = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Give the third number:");
-        int third = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("Hello Metropolia !");
     }
 }
+
+
 
