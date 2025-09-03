88299a8b98d6c5702abaa9fc252fc62dcce30e4a
Chathurika <silvachathurtika@outlook.com>
2025-09-03 14:40:55 +0300

    Triangle

--- Assignmnet3point1/src/Triangle.java
+++ Assignmnet3point1/src/Triangle.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class Triangle extends Shape {
+    private double base;
+    private double height;
 
-public class Traingle {
+    public Triangle(double base, double height, String color) {
+        super(color);
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle with base " + base + ", height " + height + ", color: " + color;
+    }
 }
+
+
