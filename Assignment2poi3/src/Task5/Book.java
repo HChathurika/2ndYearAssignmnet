7dd2816406e43e9eafe16f05006d2eff7fdad6d7
Chathurika <silvachathurtika@outlook.com>
2025-09-04 09:57:22 +0300

    Task5

---
+++ Assignment2poi3/src/Task5/Book.java
@@ -0,0 +1,71 @@
+package Task5;
+
+import java.util.ArrayList;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean isBorrowed;
+
+    private double rating;  // new field
+    private ArrayList<String> reviews; // new field
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isBorrowed = false;
+        this.rating = 0.0;
+        this.reviews = new ArrayList<>();
+    }
+
+    // getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    // borrow/return
+    public void borrowBook() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+
+    // rating
+    public void setRating(double rating) {
+        if (rating >= 0 && rating <= 5) {
+            this.rating = rating;
+        } else {
+            System.out.println("Rating must be between 0 and 5.");
+        }
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    // reviews
+    public void addReview(String review) {
+        reviews.add(review);
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+}
+
