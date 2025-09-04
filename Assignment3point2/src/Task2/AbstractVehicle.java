1b2457510b794b081f6f8584274d54946604bd95
Chathurika <silvachathurtika@outlook.com>
2025-09-04 10:19:33 +0300

    task2

--- Assignment3point2/src/Task2/AbstractVehicle.java
+++ Assignment3point2/src/Task2/AbstractVehicle.java
@@ -1,4 +1,29 @@
 package Task2;
 
-public class AbstarctVehicle {
+
+public abstract class AbstractVehicle implements Vehicle {
+    protected String type;
+    protected String fuel;
+    protected String color;
+
+    public AbstractVehicle(String type, String fuel, String color) {
+        this.type = type;
+        this.fuel = fuel;
+        this.color = color;
+    }
+
+    @Override
+    public void start() {
+        System.out.println(type + " is starting...");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println(type + " is stopping...");
+    }
+
+    // getInfo will be implemented by subclasses if they have extra info
+    @Override
+    public abstract String getInfo();
 }
+
